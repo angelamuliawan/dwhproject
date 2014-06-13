3cb854c3db010ecc0b341e4acc16859ea626434e
amuliawan <amuliawan93@gmail.com>
2014-06-13 18:45:57 +0800

    commit manage access

--- application/views/contents/manageAccess.php
+++ application/views/contents/manageAccess.php
@@ -2,12 +2,12 @@
 	<!-- Content Header (Page header) -->
 	<section class="content-header">
 		<h1>
-			Dashboard
-			<small>Control panel</small>
+			Manage Access
+			<small>User Access Management</small>
 		</h1>
 		<ol class="breadcrumb">
 			<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-			<li class="active">Dashboard</li>
+			<li class="active">Manage Access</li>
 		</ol>
 	</section>
 
@@ -16,122 +16,33 @@
 
 		<!-- Small boxes (Stat box) -->
 		<div class="row">
-			<?php
-			 if(strstr($this->session->userdata('accessright'),'all')|| strstr($this->session->userdata('accessright'),'summaryPurchase'))
-             {
-             ?>
-			<div class="col-lg-3 col-xs-6">
-				<!-- small box -->
-				<div class="small-box bg-aqua" id="btnSummaryPurchase">
-					<div class="inner">
-						<h3>
-							2014
-						</h3>
-						<p>
-							Purchase Summary 2014
-						</p>
-					</div>
-					<div class="icon">
-						<i class="ion ion-bag"></i>
-					</div>
-					<a href="../dwhproject/summary/summaryPurchase" class="small-box-footer">
-						More info <i class="fa fa-arrow-circle-right"></i>
-					</a>
-				</div>
-			</div><!-- ./col -->
-			<?php
-			}
-			if(strstr($this->session->userdata('accessright'),'all')|| strstr($this->session->userdata('accessright'),'summarySales'))
-            {
-			?>
-			<div class="col-lg-3 col-xs-6">
-				<!-- small box -->
-				<div class="small-box bg-blue" id="btnSummarySales">
-					<div class="inner">
-						<h3>
-							2014
-						</h3>
-						<p>
-							Sales Summary 2014
-						</p>
-					</div>
-					<div class="icon">
-						<i class="ion ion-ios7-cart-outline"></i>
-					</div>
-					<a href="../dwhproject/summary/summarySales" class="small-box-footer">
-						More info <i class="fa fa-arrow-circle-right"></i>
-					</a>
-				</div>
-			</div><!-- ./col -->
-			<?php
-			}
-			if(strstr($this->session->userdata('accessright'),'all')|| strstr($this->session->userdata('accessright'),'summaryLease'))
-            {
-			?>
-			<div class="col-lg-3 col-xs-6">
-				<!-- small box -->
-				<div class="small-box bg-green" id="btnSummaryLease">
-					<div class="inner">
-						<h3>
-							2014
-						</h3>
-						<p>
-							Lease Summary 2014
-						</p>
-					</div>
-					<div class="icon">
-						<i class="ion ion-stats-bars"></i>
-					</div>
-					<a href="../dwhproject/summary/summaryLease" class="small-box-footer">
-						More info <i class="fa fa-arrow-circle-right"></i>
-					</a>
-				</div>
-			</div><!-- ./col -->
-			<?php
-			}
-			if(strstr($this->session->userdata('accessright'),'all')|| strstr($this->session->userdata('accessright'),'summaryService'))
-            {
-			?>
-			<div class="col-lg-3 col-xs-6">
-				<!-- small box -->
-				<div class="small-box bg-red" id="btnSummaryService">
-					<div class="inner">
-						<h3>
-							2014
-						</h3>
-						<p>
-							Service Summary 2014
-						</p>
-					</div>
-					<div class="icon">
-						<i class="ion ion-pie-graph"></i>
-					</div>
-					<a href="../dwhproject/summary/summaryService" class="small-box-footer">
-						More info <i class="fa fa-arrow-circle-right"></i>
-					</a>
-				</div>
-			</div><!-- ./col -->
-			<?php
-			}
-			?>
+			 <!--Table -->
+			 <table id="tblAccess" style="width:70%;margin-left:10px;" border="1">
+			  	<thead>
+				    <tr>
+				    	<th style="text-align:center;">User Name</th>
+				    	<th style="text-align:center;">Page Name</th>
+					    <th style="text-align:center;">Action</th>
+				  	</tr>
+				</thead>
+				<tbody>
+					<tr id="iTemplateAcc"style="display:none" class="loop">
+						<td class="iUser" style="text-align:center;">
+							<select id="ddlUser"></select>
+						</td>
+						<td class="iPage" style="text-align:center;">
+							<select id="ddlPage"></select>
+						</td>
+						<td class="iAction" style="text-align:center;">
+							<a style="cursor:pointer;" class="btnDeleteAccess">Delete</a>
+						</td>
+					</tr>
+				 </tbody>
+			</table>
+			<!-- End Of Table -->
 		</div><!-- /.row -->
 
 	</section><!-- /.content -->
 </aside><!-- /.right-side -->
 
-<script type="text/javascript">
-
-	$("#btnSummaryPurchase").click(function(){
-		location.href = "../dwhproject/summary/summaryPurchase";
-	});
-	$("#btnSummaryService").click(function(){
-		location.href = "../dwhproject/summary/summaryService";
-	});
-	$("#btnSummaryLease").click(function(){
-		location.href = "../dwhproject/summary/summaryLease";
-	});
-	$("#btnSummarySales").click(function(){
-		location.href = "../dwhproject/summary/summarySales";
-	});
-
-</script>
+<script src="../../dwhproject/media/js/ContentJS/manageAccess.js"></script>
--- media/js/ContentJS/manageAccess.js
+++ media/js/ContentJS/manageAccess.js
@@ -1,21 +1,99 @@
 $(document).ready(function(){
-	$('#loginForm').submit(function(e){
-		e.preventDefault();
-		var loginParam = $(this).serializeJSON();
-		//loginParam.editnim = psv.id;
+	AB.ajax({
+		url: AB.serviceUri + 'manageAccess/getUserName',
+		type: 'post',
+		dataType: 'json',
+		contentType: 'application/json;charset=utf-8',
+		success:function(data){
+			for(var i=0; i<data.length; i++)
+			{
+				$('#ddlUser').append(
+					'<option value='+data[i].UserID+'>'+data[i].UserName+'</option>'
+				)
+			}
+		}
+	});
+
+	AB.ajax({
+		url: AB.serviceUri + 'manageAccess/getPageName',
+		type: 'post',
+		dataType: 'json',
+		contentType: 'application/json;charset=utf-8',
+		success:function(data){
+			for(var i=0; i<data.length; i++)
+			{
+				$('#ddlPage').append(
+					'<option value='+data[i].PageID+'>'+data[i].PageID+'</option>'
+				)
+			}
+			loadAccess();
+		}
+	});
+
+	$("body").on('click', '#btnSaveAccess',function(){
+
+		var userid = $('#ddlUser').find('option:selected').text();
+		var pageid = $('#ddlPage').find('option:selected').text();
+
+		alert(userid);
+		alert(pageid);
+
 		AB.ajax({
-			url: AB.serviceUri + 'login/loginUser',
+			url: AB.serviceUri + 'manageAccess/insertUserAccess',
 			type: 'post',
 			dataType: 'json',
+			data : JSON.stringify({pageid : pageid, userid : userid}),
 			contentType: 'application/json;charset=utf-8',
-			data: JSON.stringify(loginParam),
-			success:function(data){
-				if(data.status == 'success') {
-					location.href = AB.dashboardUri;
-				} else {
-					alert('Login failed');
-				}
+			success:function(data){						
+				alert("Item successfully added.");
+				loadAccess();
 			}
 		});
 	});
+
 });
+
+function loadAccess(){
+	AB.ajax({
+		url: AB.serviceUri + 'manageAccess/getAccess',
+		type: 'post',
+		dataType: 'json',
+		contentType: 'application/json;charset=utf-8',
+		success:function(data){
+			var table = $("#tblAccess");
+			$("tbody",table).find("tr").not("#iTemplateAcc").remove();
+			for(var i = 0; i<data.length; i++)
+			{
+				var newRow = $("#iTemplateAcc",table).clone().css("display","").removeAttr("id");
+				$(".iUser",newRow).empty().text(data[i].UserName).attr('data-id', data[i].UserID);
+				$(".iPage",newRow).empty().text(data[i].PageID);
+				$(".btnDeleteAccess",newRow).attr('data-id',data[i].PageID).click(function(){
+
+					var conf = confirm("Do you want to delete this item ?");
+					if(conf){
+						var pageid = $(this).closest("tr").find(".btnDeleteAccess").attr('data-id');
+						var userid = $(this).closest("tr").find(".iUser").attr('data-id');
+						AB.ajax({
+							url: AB.serviceUri + 'manageAccess/deleteUserAccess',
+							type: 'post',
+							dataType: 'json',
+							data : JSON.stringify({pageid : pageid, userid : userid}),
+							contentType: 'application/json;charset=utf-8',
+							success:function(data){						
+								alert("Item successfully deleted.");
+								loadAccess();
+							}
+						});
+					}
+				});
+
+
+				$("tbody",table).append(newRow);
+			}
+			var tmp = $("#iTemplateAcc").clone().css("display", "").removeAttr("id").removeClass("loop").addClass("datarow");
+			
+			$(".iAction",tmp).empty().append('<a style="cursor:pointer;" id="btnSaveAccess">Save</a>');
+			$("#tblAccess tbody").append(tmp);
+		}
+	});
+}
